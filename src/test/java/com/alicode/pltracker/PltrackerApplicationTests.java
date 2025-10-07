879f24a8e86c7eb5bdadee7e2cfa2855c74a1151
ali <alichocr2@gmail.com>
2025-10-07 14:15:59 +0100

    initial commit

---
+++ src/test/java/com/alicode/pltracker/PltrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alicode.pltracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PltrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
